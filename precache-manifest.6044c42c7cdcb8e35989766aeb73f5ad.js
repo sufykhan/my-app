d9e5059959cebe3541e92a47b663b927b1e6893f
Almighty Stranger <sufyankhan9678@gmail.com>
2020-08-03 16:22:45 +0530

    Updates

---
+++ precache-manifest.6044c42c7cdcb8e35989766aeb73f5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0000adda9cc969d9d1dd80de7176e0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "88638c1ab5c711c10c6a",
+    "url": "/my-app/static/css/main.e9430660.chunk.css"
+  },
+  {
+    "revision": "9400cf6fb8621fbae444",
+    "url": "/my-app/static/js/2.1ed344d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1ed344d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88638c1ab5c711c10c6a",
+    "url": "/my-app/static/js/main.4b15f8d5.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
